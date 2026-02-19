e7ccaa31a889cd7294070b1b9d741e74e7ed12fa
akonovalov <alexberkut@gmail.com>
2026-02-19 21:28:14 +0300

    Ad debug. Fixing Hibernate 6 issue with @Lob

--- resource-service/src/main/java/com/epam/resourceservice/entity/Resource.java
+++ resource-service/src/main/java/com/epam/resourceservice/entity/Resource.java
@@ -4,7 +4,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Lob;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -21,6 +20,5 @@ public class Resource {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Lob
     private byte[] data;
 }
